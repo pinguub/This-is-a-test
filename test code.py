ed2de5d1d2b36008c3570adfa50dd10699ac3d80
pinguub <1553059601@qq.com>
2023-11-29 12:10:51 +0800

    pin
    评论一下

---
+++ test code.py
@@ -0,0 +1,9 @@
+# --*-- coding:utf-8 --*--
+# @Time : 2023/11/27 23:51
+# @Author : pingu
+# @Email : 19zwlin2@stu.edu.cn
+# @File : test code.py
+# @Software : PyCharm
+X = [1,2,3,4,5,6,7]
+a = X.shape
+print(a)
